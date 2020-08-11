7fd5115e21665b8b2b26233f751a5fc764ca2d69
acronhub <shiratama.make@gmail.com>
2020-08-11 23:37:02 +0900

    ffmpeg 進捗率表示 修正

--- apps/static/js/main.js
+++ apps/static/js/main.js
@@ -2,9 +2,23 @@ function getLog() {
     $.ajax({
      url: '/progress',
      dataType: 'text',
-     success: function(text) {
-      console.log(text);
-      setTimeout(getLog, 1000); //refresh every 1 seconds 
+     success: function(json) {
+        const obj = JSON.parse(json);
+        const time = parseInt(obj.time);
+        if (time != -1) {
+            // プログレスバー表示
+            document.getElementById("progress-convert").style.display = "flex";
+            document.getElementById("form-convert").style.display = "none";
+
+            document.getElementById("progress-bar-convert").style.width = time + '%';
+            document.getElementById("progress-bar-convert").innerHTML = time + '%';
+        } else {
+            // フォーム 表示
+            document.getElementById("progress-convert").style.display = "none";
+            document.getElementById("form-convert").style.display = "block";
+        }
+
+        setTimeout(getLog, 1000); //refresh every 1 seconds 
      }
     })
 }
